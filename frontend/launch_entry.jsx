ce48bdcfd625f76b5265f20e865ab3f0ac1708f9
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-01-02 15:19:06 -0500

    added configureStore to window for testing and added two files App.jsx and root.jsx to frontend components

--- frontend/launch_entry.jsx
+++ frontend/launch_entry.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 const store = configureStore();
 
+console.log(store);
 window.getState = store.getState;
 window.dispatch = store.dispatch;
 
