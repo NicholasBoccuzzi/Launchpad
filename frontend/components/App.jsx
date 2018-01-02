9a34e2d8bc6b7fe4ae3fccbf7969c0363851ebb2
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-01-02 17:51:48 -0500

    added route, redirect, switch, link, hashrouter to App.jsx

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import SessionFormContainer from './session_form_container';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
 
 const App = () => (
   <div>
